69758a887f7d3881b247841b593808a2031706a0
gegehprast <gegeh.prast@gmail.com>
2019-07-27 16:09:19 +0800

    updated tetew smh; decode url params

--- utils/utils.js
+++ utils/utils.js
@@ -18,44 +18,40 @@ class Util {
         return new Promise(resolve => setTimeout(resolve, ms))
     }
 
+    base64Decode(string) {
+        const buff = Buffer.from(string, 'base64')
+        return buff.toString('ascii')
+    }
+
     /**
      * Get all query parameter from a url
      * @param url url 
      */
-    getAllUrlParams(url) {
-        // get query string from url (optional) or window
-        let queryString = url ? url.split('?')[1] : window.location.search.slice(1)
-
-        // we'll store the parameters here
+    getAllUrlParams(url, toLowerCase = false) {
+        let queryString = url.split('?')[1]
         let obj = {}
-
-        // if query string exists
+        
         if (queryString) {
-
-            // stuff after # is not part of query string, so get rid of it
             queryString = queryString.split('#')[0]
-
-            // split our query string into its component parts
             let arr = queryString.split('&')
 
             for (let i = 0; i < arr.length; i++) {
-                // separate the keys and the values
-                let a = arr[i].split('=')
-
-                // set parameter name and value (use 'true' if empty)
+                let a = arr[i].split('=') // separate the keys (param name) and the values (param value)
                 let paramName = a[0]
                 let paramValue = typeof (a[1]) === 'undefined' ? true : a[1]
 
-                // (optional) keep case consistent
-                paramName = paramName.toLowerCase()
-                if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase()
+                if (toLowerCase) {
+                    paramName = paramName.toLowerCase()
+                    if (typeof paramValue === 'string')
+                        paramValue = paramValue.toLowerCase()
+                }
 
                 // if the paramName ends with square brackets, e.g. colors[] or colors[2]
                 if (paramName.match(/\[(\d+)?\]$/)) {
-
                     // create key if it doesn't exist
                     let key = paramName.replace(/\[(\d+)?\]/, '')
-                    if (!obj[key]) obj[key] = []
+                    if (!obj[key])
+                        obj[key] = []
 
                     // if it's an indexed array e.g. colors[2]
                     if (paramName.match(/\[\d+\]$/)) {
