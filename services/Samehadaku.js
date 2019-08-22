d0fca09ca088cf3d7bd35850f5c9cdd5a672044c
gegehprast <gegeh.prast@gmail.com>
2019-08-22 08:58:35 +0000

    idk

--- services/Samehadaku.js
+++ services/Samehadaku.js
@@ -197,7 +197,7 @@ class Samehadaku {
                             const link = await anchor.getProperty('href').then(async property => await property.jsonValue())
 
                             downloadLinks.push({
-                                quality: `${quality}${format}`,
+                                quality: `${format} ${quality}`,
                                 host: host,
                                 link: link
                             })
