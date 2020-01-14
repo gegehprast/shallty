3c1e78aa34983b8eada7dbe02126c2ff32209357
gegehprast <gegeh.prast@gmail.com>
2020-01-14 19:50:15 +0800

    updated axeha

--- shortlinks/Ahexa.js
+++ shortlinks/Ahexa.js
@@ -14,6 +14,17 @@ class Ahexa {
             link = decodeURIComponent(link)
             await page.goto(link)
 
+            await Util.sleep(3000)
+            const url = page.url()
+
+            if (!url.includes('axeha')) {
+                await page.close()
+
+                return {
+                    url: url
+                }
+            }
+
             await page.waitForSelector('div.download-link')
             const div = await page.$('div.download-link')
             const raw = await div.$eval('a', node => node.href)
