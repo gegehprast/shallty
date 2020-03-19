12622e82b12fabbf65f327492fc5ac3bf4c026af
gegehprast <gegeh.prast@gmail.com>
2020-03-20 00:08:25 +0800

    fixed param.fantl

--- Socket/listeners/fantl.js
+++ Socket/listeners/fantl.js
@@ -4,31 +4,31 @@ const fantls = {
 
 module.exports = {
     mangaList: async (io, socket, param) => {
-        const data = await fantls[param.fansub].mangaList()
+        const data = await fantls[param.fantl].mangaList()
 
         io.emit('mangaList', data)
     },
 
     mangaInfo: async (io, socket, param) => {
-        const data = await fantls[param.fansub].mangaInfo(param.link)
+        const data = await fantls[param.fantl].mangaInfo(param.link)
 
         io.emit('mangaInfo', data)
     },
 
     chapters: async (io, socket, param) => {
-        const data = await fantls[param.fansub].chapters(param.link)
+        const data = await fantls[param.fantl].chapters(param.link)
 
         io.emit('chapters', data)
     },
 
     images: async (io, socket, param) => {
-        const data = await fantls[param.fansub].images(param.link)
+        const data = await fantls[param.fantl].images(param.link)
 
         io.emit('images', data)
     },
 
     newReleases: async (io, socket, param) => {
-        const data = await fantls[param.fansub].newReleases()
+        const data = await fantls[param.fantl].newReleases()
 
         io.emit('newReleases', data)
     },
