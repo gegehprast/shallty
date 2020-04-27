50baecfbb0d5cc4ef05f33b802cc9e021f7b9990
gegehprast <gegeh.prast@gmail.com>
2020-04-27 23:30:21 +0800

    cleaning up controllers and listeners

--- Socket/listeners/shortlink.js
+++
@@ -1,9 +0,0 @@
-const Shortlink =  require('../../shortlinks')
-
-module.exports = {
-    parse: async (io, socket, param) => {
-        const data = await Shortlink.parse(param.link)
-
-        io.emit('parse', data)
-    },
-}
