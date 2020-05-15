65d100ab079a0c524d9f4c06eeeb7d55cb7bfbb7
gegehprast <gegeh.prast@gmail.com>
2020-05-15 14:10:18 +0800

    cleaning up

--- Socket/emitters/A.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-module.exports = (io, socket, data) => {
-    io.emit('a', {
-        msg: 'Hello A!',
-    })
-}
--- Socket/emitters/B.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-module.exports = (io, socket, data) => {
-    io.emit('b', {
-        msg: 'Hello B!',
-    })
-}
