f39e7cce7d647d5b9f2a5015517f48d84f6ae62c
gegehprast <gegeh.prast@gmail.com>
2021-07-10 14:48:04 +0700

    shortlink listener

---
+++ src/socket/listeners/shortlink.ts
@@ -0,0 +1,12 @@
+import { Namespace, Socket } from 'socket.io'
+import ParserManager from '../../parsers/ParserManager'
+
+const listeners = (io: Namespace, socket: Socket) => {
+    socket.on('parse', async function (params) {
+        const data = await ParserManager.parse(params.link)
+
+        io.to(socket.id).emit('parse', data)
+    })
+}
+
+export default listeners
