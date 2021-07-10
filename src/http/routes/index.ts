f15fc2c54f607fcdfdab22dc560118c6d7981208
gegehprast <gegeh.prast@gmail.com>
2021-07-10 14:28:32 +0700

    shortlink > parse

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -2,6 +2,6 @@ import express from 'express'
 import ParserController from '../controllers/ParserController'
 const api = express.Router()
 
-api.get('/shortlink', ParserController.index)
+api.get('/parse', ParserController.index)
 
 export default api
