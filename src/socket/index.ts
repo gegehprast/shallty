4089419204572c2536ded1ebd5ce61a1129eb4cb
gegehprast <gegeh.prast@gmail.com>
2021-07-10 14:48:09 +0700

    shortlinkListener

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,19 +1,22 @@
 import http from 'http'
 import { Server } from 'socket.io'
+import shortlinkListener from './listeners/shortlink'
 
 const Init = (server: http.Server) => {
     const io = new Server(server)
     const shortlink = io.of('/shortlink')
 
     shortlink.on('connection', function (socket) {
-        console.log('Connected to shortlink')
+        console.info('\x1b[34m%s\x1b[0m', '[Websocket] Connected. Namespace: shortlink')
 
         socket.on('disconnect', function (reason) {
-            console.log(reason)
+            console.info('\x1b[34m%s\x1b[0m', `[Websocket] Disconnected. Reason: ${reason}`)
         })
+
+        shortlinkListener(shortlink, socket)
     })
 
-    console.info('\x1b[34m%s\x1b[0m', `[Websocket Server Ready] You can access it at http://localhost:${process.env.APP_PORT || 3000}`)
+    console.info('\x1b[34m%s\x1b[0m', `[Websocket] Server Ready. You can access it at http://localhost:${process.env.APP_PORT || 3000}`)
 }
 
 export default Init
