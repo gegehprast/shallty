c20cd1a8136539d5cda4ef57de220e415a2a0efd
gegehprast <gegeh.prast@gmail.com>
2021-07-10 14:53:14 +0700

    update msg

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -7,10 +7,10 @@ const Init = (server: http.Server) => {
     const shortlink = io.of('/shortlink')
 
     shortlink.on('connection', function (socket) {
-        console.info('\x1b[34m%s\x1b[0m', '[Websocket] Connected. Namespace: shortlink')
+        console.info('\x1b[34m%s\x1b[0m', '[Websocket] A client has been connected. Namespace: shortlink')
 
         socket.on('disconnect', function (reason) {
-            console.info('\x1b[34m%s\x1b[0m', `[Websocket] Disconnected. Reason: ${reason}`)
+            console.info('\x1b[34m%s\x1b[0m', `[Websocket] A client has been disconnected. Reason: ${reason}`)
         })
 
         shortlinkListener(shortlink, socket)
