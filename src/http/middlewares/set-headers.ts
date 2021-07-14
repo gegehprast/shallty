8a6d80ee1d141c463ba7158ba094a61035129456
gegehprast <gegeh.prast@gmail.com>
2021-07-14 15:14:28 +0700

    allow all origin

--- src/http/middlewares/set-headers.ts
+++ src/http/middlewares/set-headers.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express'
 
 const setHeaders: RequestHandler = (req, res, next) => {
     res.set('X-Powered-By', 'Shallty\'s Love')
-    res.append('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS)
+    res.append('Access-Control-Allow-Origin', '*')
     res.append('Access-Control-Allow-Credentials', 'true')
     res.append('Access-Control-Allow-Methods', 'GET')
     next()
