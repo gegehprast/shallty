73777d6619673d16303cc4c205e3800b24df7000
gegehprast <gegeh.prast@gmail.com>
2021-07-25 13:26:31 +0700

    getUrlQueryParam

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -58,62 +58,13 @@ class Util {
     }
 
     /**
-     * Get all query parameter from a url
+     * Get a query parameter from a url string.
      * 
      */
-    getAllUrlParams(url: string, toLowerCase = false) {
-        let queryString = url.split('?')[1]
-        const obj: any = {}
+    getUrlQueryParam(url_string: string, param: string) {
+        const url = new URL(url_string)
 
-        if (queryString) {
-            queryString = queryString.split('#')[0]
-            const arr = queryString.split('&')
-
-            for (let i = 0; i < arr.length; i++) {
-                const a = arr[i].split('=') // separate the keys (param name) and the values (param value)
-                let paramName = a[0]
-                let paramValue = typeof (a[1]) === 'undefined' ? true : a[1]
-
-                if (toLowerCase) {
-                    paramName = paramName.toLowerCase()
-                    if (typeof paramValue === 'string')
-                        paramValue = paramValue.toLowerCase()
-                }
-
-                // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-                if (paramName.match(/\[(\d+)?\]$/)) {
-                    // create key if it doesn't exist
-                    const key = paramName.replace(/\[(\d+)?\]/, '')
-                    if (!obj[key])
-                        obj[key] = []
-
-                    // if it's an indexed array e.g. colors[2]
-                    if (paramName.match(/\[\d+\]$/)) {
-                        // get the index value and add the entry at the appropriate position
-                        const index = /\[(\d+)\]/.exec(paramName)[1]
-                        obj[key][index] = paramValue
-                    } else {
-                        // otherwise add the value to the end of the array
-                        obj[key].push(paramValue)
-                    }
-                } else {
-                    // we're dealing with a string
-                    if (!obj[paramName]) {
-                        // if it doesn't exist, create property
-                        obj[paramName] = paramValue
-                    } else if (obj[paramName] && typeof obj[paramName] === 'string') {
-                        // if property does exist and it's a string, convert it to an array
-                        obj[paramName] = [obj[paramName]]
-                        obj[paramName].push(paramValue)
-                    } else {
-                        // otherwise add the property
-                        obj[paramName].push(paramValue)
-                    }
-                }
-            }
-        }
-
-        return obj
+        return url.searchParams.get(param)
     }
 
     /**
